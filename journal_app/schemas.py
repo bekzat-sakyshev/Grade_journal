9adb7677df9b9c41c6269f882177fbe3f36d86f0
Сақышев Бекзат <iitu.bekzat@gmail.com>
2024-05-31 02:59:32 +0500

    create project journal_app in FastApi and SqlAlchemy

---
+++ journal_app/schemas.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, Field
+
+
+class StudentBase(BaseModel):
+    name: str
+
+
+class StudentCreate(StudentBase):
+    pass
+
+
+class Student(StudentBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class ScoreBase(BaseModel):
+    value: int
+    student_id: int
+
+
+class ScoreCreate(ScoreBase):
+    pass
+
+
+class Score(ScoreBase):
+    id: int
+
+    class Config:
+        orm_mode = True
