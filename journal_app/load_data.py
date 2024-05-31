98fbd6968320e7d778ddd95acde1d12e3e7cbf88
Сақышев Бекзат <iitu.bekzat@gmail.com>
2024-05-31 05:18:15 +0500

    added instructions for starting the project and API documentation

--- journal_app/load_data.py
+++ journal_app/load_data.py
@@ -7,14 +7,14 @@ import crud
 
 def load_data():
     db = SessionLocal()
-    
+
     students = [
         schemas.StudentCreate(name="John Doe"),
         schemas.StudentCreate(name="Jane Smith"),
     ]
     for student in students:
         crud.create_student(db=db, student=student)
-    
+
     db.close()
 
 
